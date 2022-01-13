e16969107cb6adbf610a40e09cfed151be99480b
Sachin Kumar <sachin.kumar@infostride.com>
2022-01-13 13:00:10 +0530

    Sele

--- First.java
+++ First.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class First {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\SachinKumar\\Desktop\\Helping files\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\SachinKumar\\Desktop\\Helping files\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         driver.quit();
--- V1P1.java
+++ V1P1.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -5,12 +6,14 @@ import java.time.Duration;
 
 public class V1P1 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/SachinKumar/Desktop/Helping files/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "C:/Users/SachinKumar/Desktop/Helping files/chromedriver.exe");
+//        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(1));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(1));
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.get("https://google.com");
-        driver.close();
+//        driver.close();
     }
 }
