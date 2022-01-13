56914c6d0e9c51bd2cd367a5e790f844ffc44517
Sachin Kumar <sachin.kumar@infostride.com>
2022-01-13 12:24:08 +0530

    Sele

--- V1P1.java
+++ V1P1.java
@@ -1,10 +1,16 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class V1P1 {
     public static void main(String[] args) {
-        System
+        System.setProperty("webdriver.chrome.driver", "C:/Users/SachinKumar/Desktop/Helping files/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(1));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.get("https://google.com");
+        driver.close();
     }
 }
