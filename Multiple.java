b138d111666cabd11a834971d7784fec0eef7c56
Sachin Kumar <sachin.kumar@infostride.com>
2022-01-17 17:33:06 +0530

    Sele

---
+++ Multiple.java
@@ -0,0 +1,24 @@
+interface Iface1{
+    default void show(){
+        System.out.println("I am Interface1");
+    }
+}
+
+interface Iface2{
+    default void show(){
+        System.out.println("I am Interface2");
+    }
+}
+
+
+public class Multiple implements Iface2, Iface1{
+    public void show() {
+        Iface1.super.show();
+        Iface2.super.show();
+    }
+
+    public static void main(String[] args) {
+        Multiple ml =new Multiple();
+        ml.show();
+    }
+}
---
+++ Paledrom.java
@@ -0,0 +1,14 @@
+public class Paledrom {
+    public static void main(String[] args) {
+        String s1="hiih";
+        StringBuffer s2;
+        s2 = new StringBuffer(s1);
+        s2.reverse();
+        System.out.println(s1+" "+s2);
+        if(s1.equals(s2.toString())){
+            System.out.println("It is a palindrome....");
+        }else{
+            System.out.printf("It is not a palindrome....");
+        }
+    }
+}
