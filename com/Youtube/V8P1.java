f1be9935fa1382a97a026288ebf633f4c075a31f
Sachin Kumar <sachin.kumar@infostride.com>
2022-01-19 20:01:08 +0530

    Selenium codes

--- com/Youtube/V8P1.java
+++ com/Youtube/V8P1.java
@@ -1,4 +1,4 @@
-package com.OrangeHRM;
+package com.Youtube;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -6,10 +6,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
-public class KeyDown {
+public class V8P1 {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
---
+++ com/Youtube/V9P1.java
@@ -0,0 +1,23 @@
+package com.Youtube;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class V9P1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://selenium08.blogspot.com/2020/01/click-and-hold.html");
+        WebElement BoxA = driver.findElement(By.xpath("//li[@name='A']"));
+        WebElement BoxD = driver.findElement(By.xpath("//li[@name='D']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(BoxA);
+        actions.build().perform();
+
+    }
+}
